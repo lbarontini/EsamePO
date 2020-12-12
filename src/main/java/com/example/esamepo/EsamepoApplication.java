24ee710b1785eba6a429d3007a4156a9cf08cc7b
Orazio <orazioedoardo@users.noreply.github.com>
2020-12-12 14:12:40 +0100

    first commit

---
+++ src/main/java/com/example/esamepo/EsamepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.esamepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsamepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsamepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/esamepo/EsamepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esamepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsamepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
